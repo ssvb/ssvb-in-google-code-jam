dc37bcd83345d788db1479668eceeae466db9e04
Siarhei Siamashka <siarhei.siamashka@gmail.com>
2015-04-19 03:24:18 +0300

    Round 1A 2015, Problem C, second code optimization
    Split the forest into a hierarchy of rectangular zones
    and run checks against these zones first. If all four
    corners of a zone are located at one side of a line,
    then all the trees inside of this zone are located
    on that side of a line too, avoiding the need to check
    all these trees individually.
    
    Benchmark on AMD Phenom(tm) 8450, running at 2.1GHz:
    
    == before ==
    
    real    7m45.800s
    user    7m45.736s
    sys     0m0.244s
    
    == after ==
    
    real	2m29.604s
    user	2m30.146s
    sys	0m0.326s

--- 2015/1A/C-try3.cr
+++ 2015/1A/C-try3.cr
@@ -3,6 +3,106 @@
 # (statically typed language, based on Ruby syntax)    #
 ########################################################
 
+PIXTHRESHOLD = 50
+SUBZONETHRESHOLD = 16
+
+def analyze_treelist(res, treelist, p1, p2, mincount)
+    mup = (p2[1] - p1[1])
+    mdown = (p2[0] - p1[0])
+    bconst = p1[1] * mdown - p1[0] * mup
+    if p2[0] == p1[0]
+      treelist.each {|tree|
+          if tree[0] < p1[0]
+            res[0] += 1
+            return if res[0] >= mincount && res[1] >= mincount
+          elsif tree[0] > p1[0]
+            res[1] += 1
+            return if res[0] >= mincount && res[1] >= mincount
+          end
+      }
+    elsif p2[1] == p1[1]
+      treelist.each {|tree|
+          if tree[1] < p1[1]
+            res[0] += 1
+            return if res[0] >= mincount && res[1] >= mincount
+          elsif tree[1] > p1[1]
+            res[1] += 1
+            return if res[0] >= mincount && res[1] >= mincount
+          end
+      }
+    else
+      treelist.each {|tree|
+        y = tree[0] * mup + bconst
+        if y > tree[1] * mdown
+            res[0] += 1
+            return if res[0] >= mincount && res[1] >= mincount
+        elsif y < tree[1] * mdown
+            res[1] += 1
+            return if res[0] >= mincount && res[1] >= mincount
+        end
+      }
+    end
+end
+
+class ForestZone
+  def initialize(treelist)
+    xcoord = treelist.map {|pixel| pixel[0] }
+    ycoord = treelist.map {|pixel| pixel[1] }
+    x1 = xcoord.min
+    y1 = ycoord.min
+    x2 = xcoord.max
+    y2 = ycoord.max
+    @extents = [{x1, y1}, {x1, y2}, {x2, y1}, {x2, y2}]
+    @treelist = treelist.shuffle
+    @subzones = [] of ForestZone
+    @scratch = [0, 0]
+    if @treelist.size > PIXTHRESHOLD
+      tmp = [@treelist]
+      tmp.sort! {|a, b| b.size <=> a.size }
+      while tmp[0].size > PIXTHRESHOLD && tmp.size < SUBZONETHRESHOLD
+        tmplist = tmp.shift
+
+        xcoord = tmplist.map {|pixel| pixel[0] }.sort
+        ycoord = tmplist.map {|pixel| pixel[1] }.sort
+        medianx = (xcoord.min + xcoord.max) / 2
+        mediany = (ycoord.min + ycoord.max) / 2
+
+        a = tmplist.select {|p| p[0] < medianx && p[1] < mediany}
+        b = tmplist.select {|p| p[0] >= medianx && p[1] < mediany}
+        c = tmplist.select {|p| p[0] < medianx && p[1] >= mediany}
+        d = tmplist.select {|p| p[0] >= medianx && p[1] >= mediany}
+        tmp.push(a) if a.size > 0
+        tmp.push(b) if b.size > 0
+        tmp.push(c) if c.size > 0
+        tmp.push(d) if d.size > 0
+        tmp.sort! {|a, b| b.size <=> a.size }
+      end
+      tmp.each {|treelist| @subzones.push(ForestZone.new(treelist)) }
+      @subzones.shuffle!
+    end
+  end
+
+  def analyze_zone(res, p1, p2, mincount)
+    @scratch[0] = 0
+    @scratch[1] = 0
+    analyze_treelist(@scratch, @extents, p1, p2, mincount)
+    if (@scratch[0] == 4 && @scratch[1] == 0) || ((@scratch[0] == 0 && @scratch[1] == 4))
+      res[0] += @scratch[0] * @treelist.size / 4
+      res[1] += @scratch[1] * @treelist.size / 4
+      return
+    end
+
+    if @subzones.size > 0
+      @subzones.each {|z|
+        z.analyze_zone(res, p1, p2, mincount)
+        return if res[0] >= mincount && res[1] >= mincount
+      }
+    else
+      analyze_treelist(res, @treelist, p1, p2, mincount)
+    end
+  end
+end
+
 abort "Need input file name in the command line" if ARGV.size < 1
 
 input = File.open(ARGV[0]).read.split.map {|x| x.to_i64 }
@@ -18,6 +118,8 @@ t = input.shift
   trees = [] of Tuple(Int64, Int64)
   1.upto(n) {|idx| trees.push({input.shift, input.shift}) }
 
+  zone = ForestZone.new(trees)
+
   0.upto(n - 1) {|n1|
     mincount = n
     0.upto(n - 1) {|n2|
@@ -25,38 +127,9 @@ t = input.shift
 
       p1 = trees[n1]
       p2 = trees[n2]
+
       res = [0, 0]
-
-      if p2[0] == p1[0]
-        trees.each_with_index {|tree, idx|
-          next if n1 == idx || n2 == idx
-          if tree[0] < p1[0]
-            res[0] += 1
-            break if res[0] >= mincount && res[1] >= mincount
-          elsif tree[0] > p1[0]
-            res[1] += 1
-            break if res[0] >= mincount && res[1] >= mincount
-          end
-        }
-      else
-        # line equation: y = m * x + b
-        mup = (p2[1] - p1[1])
-        mdown = (p2[0] - p1[0])
-        bconst = p1[1] * mdown - p1[0] * mup
-
-        trees.each_with_index {|tree, idx|
-          next if idx == n1
-          next if idx == n2
-          y = tree[0] * mup + bconst
-          if y > tree[1] * mdown
-            res[0] += 1
-            break if res[0] >= mincount && res[1] >= mincount
-          elsif y < tree[1] * mdown
-            res[1] += 1
-            break if res[0] >= mincount && res[1] >= mincount
-          end
-        }
-      end
+      zone.analyze_zone(res, p1, p2, mincount)
 
       mincount = [mincount, res.min].min
       break if mincount == 0
