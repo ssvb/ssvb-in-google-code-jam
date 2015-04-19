c150c8a8c4ba51c72e5a30a9b8c92928d3ebee65
Siarhei Siamashka <siarhei.siamashka@gmail.com>
2015-04-19 03:09:04 +0300

    Round 1A 2015, Problem C (Logging)
    https://code.google.com/codejam/contest/4224486/dashboard
    
    Added a naive bruteforce implementation for Crystal 0.6.1
    (http://crystal-lang.org) and also test files.
    
    Benchmark on AMD Phenom(tm) 8450, running at 2.1GHz:
    
    $ time crystal run --release C-try1.cr -- C-large-practice.in
    
    real	18m5.527s
    user	18m5.068s
    sys	0m0.352s

---
+++ 2015/1A/C-try1.cr
@@ -0,0 +1,65 @@
+########################################################
+# Crystal compiler 0.6.1 from http://crystal-lang.org/ #
+# (statically typed language, based on Ruby syntax)    #
+########################################################
+
+abort "Need input file name in the command line" if ARGV.size < 1
+
+input = File.open(ARGV[0]).read.split.map {|x| x.to_i64 }
+
+casenum = 1
+t = input.shift
+
+1.upto(t) {
+  puts "Case ##{casenum}:"
+  casenum += 1
+
+  n = input.shift.to_i
+  trees = [] of Tuple(Int64, Int64)
+  1.upto(n) {|idx| trees.push({input.shift, input.shift}) }
+
+  0.upto(n - 1) {|n1|
+    mincount = n
+    0.upto(n - 1) {|n2|
+      next if n1 == n2
+
+      p1 = trees[n1]
+      p2 = trees[n2]
+      res = [0, 0]
+
+      if p2[0] == p1[0]
+        trees.each_with_index {|tree, idx|
+          next if n1 == idx || n2 == idx
+          if tree[0] < p1[0]
+            res[0] += 1
+          elsif tree[0] > p1[0]
+            res[1] += 1
+          end
+        }
+      else
+        # line equation: y = m * x + b
+        mup = (p2[1] - p1[1])
+        mdown = (p2[0] - p1[0])
+        bconst = p1[1] * mdown - p1[0] * mup
+
+        trees.each_with_index {|tree, idx|
+          next if idx == n1
+          next if idx == n2
+          y = tree[0] * mup + bconst
+          if y > tree[1] * mdown
+            res[0] += 1
+          elsif y < tree[1] * mdown
+            res[1] += 1
+          end
+        }
+      end
+
+      mincount = [mincount, res.min].min
+    }
+    if n <= 3
+      p 0
+    else
+      p mincount
+    end
+  }
+}
